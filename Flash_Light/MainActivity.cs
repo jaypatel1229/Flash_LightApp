07470ed9e0bc97edde484b7684afbad90a0d7288
jay.patel <jay.patel@1R-80>
2022-02-08 10:23:00 +0530

    Add project files.

---
+++ Flash_Light/MainActivity.cs
@@ -0,0 +1,93 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+using Xamarin.Essentials;
+
+namespace Flash_Light
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button turnon;
+        private Button turnoff;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            UIReference();
+            UIClickEvents();
+        }
+
+        private void UIClickEvents()
+        {
+            turnoff.Click += Turnoff_Click;
+            turnon.Click += Turnon_Click;
+        }
+
+        private async void Turnon_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Turn On
+                await Flashlight.TurnOnAsync();
+
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+
+        private async void Turnoff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                // Turn Off
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+
+        private void UIReference()
+        {
+            turnon = FindViewById<Button>(Resource.Id.Onbtn);
+            turnoff = FindViewById<Button>(Resource.Id.Ofbtn);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
